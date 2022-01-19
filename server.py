3e4d3459581fc4786771b18fa48f889d06d682e4
arta asadi <2000.arta@gmail.com>
2022-01-19 19:55:50 +0330

    server basic implementations

--- server.py
+++ server.py
@@ -1,10 +1,31 @@
+from pickle import TRUE
 import socket
 import threading
 
+
+PORT = 1373 # Port to listen on
+MESSAGE_LENGTH_SIZE = 64 # Need MESSAGE_LENGTH_SIZE to get message length to know how much to recieve
+
+
 def main() :
-    pass
+    address = socket.gethostbyname(socket.gethostname()) # Get Address automatically
+    HOST_INFORMATION = (address, PORT)
+    # Initialize server socket
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server :
+        server.bind(HOST_INFORMATION)
+        start_server(server)
 
 
+def start_server(server : socket.socket) :
+    server.listen()
+    while True :
+        conn, address = server.accept()
+        t = threading.Thread(target= connection_handler, args= (conn, address))
+        t.start()
+
+def connection_handler(conn : socket.socket, address : socket._RetAddress):
+    print("[NEW CONNECTION] connected from {}".format(address))
+    conn.close()
 
 if __name__ == '__main__' :
     main()
